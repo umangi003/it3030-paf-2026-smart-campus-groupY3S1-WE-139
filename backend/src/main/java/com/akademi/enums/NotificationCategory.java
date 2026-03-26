f3659fbfe876218fe4ddbb9db66a2043f71e9ee3
Uditha Omal <uomalp@gmail.com>
2026-03-26 09:10:17 +0530

    add NotificationCategory enum

---
+++ backend/src/main/java/com/akademi/enums/NotificationCategory.java
@@ -0,0 +1,13 @@
+package com.akademi.enums;
+
+public enum NotificationCategory {
+    BOOKING_CONFIRMED,
+    BOOKING_CANCELLED,
+    BOOKING_REMINDER,
+    INCIDENT_CREATED,
+    INCIDENT_UPDATED,
+    INCIDENT_RESOLVED,
+    SLA_WARNING,
+    SLA_BREACH,
+    GENERAL
+}

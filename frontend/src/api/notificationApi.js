d6b8e180ac8de338eb1f7bc1afbb2d6680093615
Uditha Omal <uomalp@gmail.com>
2026-03-28 18:10:20 +0530

    add Notification api helpers

---
+++ frontend/src/api/notificationApi.js
@@ -0,0 +1,9 @@
+import api from './axiosInstance'
+
+export const notificationApi = {
+  getAll: () => api.get('/notifications'),
+  getUnread: () => api.get('/notifications/unread'),
+  getUnreadCount: () => api.get('/notifications/unread/count'),
+  markAsRead: (id) => api.patch(`/notifications/${id}/read`),
+  markAllAsRead: () => api.patch('/notifications/read-all'),
+}
